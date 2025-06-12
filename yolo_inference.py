0fda56267c22654ba278333a3481cbe0de7e7488
BobHell0 <anujan.cenan@gmail.com>
2025-06-12 13:31:20 +1000

    getting all bounding boxes and printing output

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,4 +1,7 @@
 from ultralytics import YOLO
 model = YOLO('yolov8x')
 
-model.predict("./input_videos/ss1_tennisMatch.png", save=True)
+results = model.predict("./input_videos/vid1_tennisMatch.mp4", save=True)
+
+for box in results[0].boxes:
+    print(box)
